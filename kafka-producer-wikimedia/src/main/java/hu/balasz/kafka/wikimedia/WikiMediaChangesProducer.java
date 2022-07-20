4e7db2609e3da7666d4fc9f5431ddfdb1599ca25
balazscsintalan <balazs.csintalan@mixiedigital.com>
2022-07-20 22:07:00 +0200

    high throughput producer configs

--- kafka-producer-wikimedia/src/main/java/hu/balasz/kafka/wikimedia/WikiMediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/hu/balasz/kafka/wikimedia/WikiMediaChangesProducer.java
@@ -28,10 +28,16 @@ public class WikiMediaChangesProducer {
     }
 
     private static Properties getProducerProperties() {
-        Properties properties = new Properties();
+        var properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOT_STRAP_SERVERS);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // high throughput producer configs
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+
         return properties;
     }
 }
